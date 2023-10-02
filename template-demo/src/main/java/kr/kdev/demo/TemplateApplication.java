ac4eec976826ed2588cdc2b7830cfaee940fec81
Mambo <kdevkr@gmail.com>
2023-10-02 23:38:46 +0900

    feature: add template-demo

---
+++ template-demo/src/main/java/kr/kdev/demo/TemplateApplication.java
@@ -0,0 +1,21 @@
+package kr.kdev.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+@SpringBootApplication
+public class TemplateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TemplateApplication.class, args);
+    }
+
+    @GetMapping("/")
+    public String index() {
+        return "index";
+    }
+
+}
